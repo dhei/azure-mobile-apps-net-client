efdf099c951f115782f2ca178c64c225d263f117
Di Hei <dihei@microsoft.com>
2016-03-03 14:44:06 -0800

    Rename directories to avoid confusion
    (1)sdk/src->src (source projects)
    (2)sdk/test->unittest (unittest projects)
    (3)e2etest/test->e2etest (e2e test projects)

---
+++ e2etest/E2ETest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("E2ETest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E2ETest")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
