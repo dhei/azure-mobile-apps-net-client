68df38eb152269a0e7ee8cb1edcbcc6f301ce41a
Taylor Southwick <tasou@microsoft.com>
2016-09-12 11:39:37 -0700

    Fix incorrect position offsets due to fixed bug in Json.NET
    See https://github.com/JamesNK/Newtonsoft.Json/issues/628 for more
    information

--- unittest/Microsoft.WindowsAzure.MobileServices.Test/UnitTests/Table/Serialization/MobileServiceSerializer.Test.cs
+++ unittest/Microsoft.WindowsAzure.MobileServices.Test/UnitTests/Table/Serialization/MobileServiceSerializer.Test.cs
@@ -323,7 +323,7 @@ namespace Microsoft.WindowsAzure.MobileServices.Test
                     actualError = e;
                 }
 
-                Assert.AreEqual(actualError.Message, "Error converting value \"I can't be parsed\" to type 'System.Boolean'. Path 'Bool', line 2, position 30.");
+                Assert.AreEqual(actualError.Message, "Error converting value \"I can't be parsed\" to type 'System.Boolean'. Path 'Bool', line 2, position 29.");
             }
         }
 
@@ -401,9 +401,9 @@ namespace Microsoft.WindowsAzure.MobileServices.Test
         public void ByteDeserializationNegative()
         {
             List<Tuple<string, string>> testCases = new List<Tuple<string, string>>() {
-                new Tuple<string, string>("{\"Byte\":256}", "Error converting value 256 to type 'System.Byte'. Path 'Byte', line 2, position 14."),
-                new Tuple<string, string>("{\"Byte\":\"256\"}","Error converting value \"256\" to type 'System.Byte'. Path 'Byte', line 2, position 16."),
-                new Tuple<string, string>("{\"Byte\":-1}","Error converting value -1 to type 'System.Byte'. Path 'Byte', line 2, position 13."),
+                new Tuple<string, string>("{\"Byte\":256}", "Error converting value 256 to type 'System.Byte'. Path 'Byte', line 2, position 13."),
+                new Tuple<string, string>("{\"Byte\":\"256\"}","Error converting value \"256\" to type 'System.Byte'. Path 'Byte', line 2, position 15."),
+                new Tuple<string, string>("{\"Byte\":-1}","Error converting value -1 to type 'System.Byte'. Path 'Byte', line 2, position 12."),
             };
 
             foreach (var testCase in testCases)
