48ae7b5a3588818bd0b0ddc0eb686c04d8619955
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-25 16:01:24 +0300

    Update Xamarin.Auth

--- src/Microsoft.Azure.Mobile.Client/Platforms/ios/Authentication/MobileServiceUIAuthentication.cs
+++ src/Microsoft.Azure.Mobile.Client/Platforms/ios/Authentication/MobileServiceUIAuthentication.cs
@@ -31,7 +31,7 @@ namespace Microsoft.WindowsAzure.MobileServices
             var tcs = new TaskCompletionSource<string>();
 
             var auth = new WebRedirectAuthenticator(StartUri, EndUri);
-            auth.ShowUIErrors = false;
+            //auth.ShowUIErrors = false;
             auth.ClearCookiesBeforeLogin = false;
 
             UIViewController c = auth.GetUI();
