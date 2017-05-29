04e3011ecf6aae327cbc03bfb3955035405d7a7c
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-29 19:25:21 +0300

    Fix parametars in login URI

--- src/Microsoft.Azure.Mobile.Client/Authentication/MobileServicePKCEAuthentication.cs
+++ src/Microsoft.Azure.Mobile.Client/Authentication/MobileServicePKCEAuthentication.cs
@@ -31,14 +31,15 @@ namespace Microsoft.WindowsAzure.MobileServices
             this.client = client;
 
             this.CodeVerifier = GetCodeVerifier();
+            this.CallbackUri = new Uri(MobileServiceUrlBuilder.CombileSchemeAndPath(uriScheme, "easyauth.callback"));
 
             var path = MobileServiceUrlBuilder.CombinePaths(LoginAsyncUriFragment, this.ProviderName);
             if (!string.IsNullOrEmpty(this.Client.LoginUriPrefix))
             {
                 path = MobileServiceUrlBuilder.CombinePaths(this.Client.LoginUriPrefix, this.ProviderName);
             }
-
             var loginParameters = parameters != null ? new Dictionary<string, string>(parameters) : new Dictionary<string, string>();
+            loginParameters.Add("post_login_redirect_url", this.CallbackUri.AbsoluteUri);
             loginParameters.Add("code_challenge", GetHash(this.CodeVerifier));
             loginParameters.Add("code_challenge_method", "S256");
             var loginQueryString = MobileServiceUrlBuilder.GetQueryString(loginParameters, useTableAPIRules: false);
@@ -49,7 +50,6 @@ namespace Microsoft.WindowsAzure.MobileServices
             {
                 this.LoginUri = new Uri(this.Client.AlternateLoginHost, loginPathAndQuery);
             }
-            this.CallbackUri = new Uri(MobileServiceUrlBuilder.CombileSchemeAndPath(uriScheme, "easyauth.callback"));
         }
         
         protected async sealed override Task<string> LoginAsyncOverride()
@@ -82,7 +82,7 @@ namespace Microsoft.WindowsAzure.MobileServices
         private static string GetHash(string data)
         {
             var sha = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha256);
-            return Encoding.UTF8.GetString(sha.HashData(Encoding.UTF8.GetBytes(data)));
+            return Convert.ToBase64String(sha.HashData(Encoding.UTF8.GetBytes(data)));
         }
     }
 }
