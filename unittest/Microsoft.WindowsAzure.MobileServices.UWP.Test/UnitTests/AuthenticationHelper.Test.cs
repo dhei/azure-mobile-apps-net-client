299a2da9994815026f722705ba2a837861f4e58e
Murat Baysangurov <v-mubays@microsoft.com>
2017-06-07 15:35:11 +0300

    AuthenticationHelper unit tests added

--- unittest/Microsoft.WindowsAzure.MobileServices.UWP.Test/UnitTests/AuthenticationHelper.Test.cs
+++ unittest/Microsoft.WindowsAzure.MobileServices.UWP.Test/UnitTests/AuthenticationHelper.Test.cs
@@ -9,7 +9,66 @@ namespace Microsoft.WindowsAzure.MobileServices.Test
 {
     [Tag("authentication")]
     [Tag("unit")]
-    class AuthenticationHelperTest:TestBase
+    public class AuthenticationHelperTest:TestBase
     {
+        [TestMethod]
+        public void OnResponseReceived_FailureUriIsNull()
+        {
+            AuthenticationHelper helper = new AuthenticationHelper();
+            Uri uri = null;
+            helper.Error += (o, e) => { Assert.IsTrue(true); };
+            helper.Completed += (o, e) => { Assert.IsTrue(false); };
+
+            helper.OnResponseReceived(uri);
+        }
+
+        [TestMethod]
+        public void OnResponseReceived_FailureUriInvalidScheme()
+        {
+            AuthenticationHelper helper = new AuthenticationHelper();
+            string uriString = "zumoe2etestapp://invalid.easyauth.callback/#authorization_code=test_code";
+            Uri uri = new Uri(uriString);
+            helper.Error += (o, e) => { Assert.IsTrue(true); };
+            helper.Completed += (o, e) => { Assert.IsTrue(false); };
+
+            helper.OnResponseReceived(uri);
+        }
+
+        [TestMethod]
+        public void OnResponseReceived_FailureUriNoAuthorizationCode()
+        {
+            AuthenticationHelper helper = new AuthenticationHelper();
+            string uriString = "zumoe2etestapp://easyauth.callback/#";
+            Uri uri = new Uri(uriString);
+            helper.Error += (o, e) => { Assert.IsTrue(true); };
+            helper.Completed += (o, e) => { Assert.IsTrue(false); };
+
+            helper.OnResponseReceived(uri);
+        }
+
+        [TestMethod]
+        public void OnResponseReceived_FailureUriParamWithNoValue()
+        {
+            AuthenticationHelper helper = new AuthenticationHelper();
+            string uriString = "zumoe2etestapp://easyauth.callback/#authorization_code=test_code&invalid_param";
+            Uri uri = new Uri(uriString);
+            helper.Error += (o, e) => { Assert.IsTrue(true); };
+            helper.Completed += (o, e) => { Assert.IsTrue(false); };
+
+            helper.OnResponseReceived(uri);
+        }
+
+        [TestMethod]
+        public void OnResponseReceived_Success()
+        {
+            AuthenticationHelper helper = new AuthenticationHelper();
+            string authorizationCode = "test_code";
+            string uriString = $"zumoe2etestapp://easyauth.callback/#authorization_code={authorizationCode}";
+            Uri uri = new Uri(uriString);
+            helper.Error += (o, e) => { Assert.IsTrue(false); };
+            helper.Completed += (o, e) => { Assert.Equals(e.AuthorizationCode, authorizationCode);};
+
+            helper.OnResponseReceived(uri);
+        }
     }
 }
