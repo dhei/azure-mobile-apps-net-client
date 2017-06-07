efeb008caa8f866361377be483c735e616d8b7fc
Murat Baysangurov <v-mubays@microsoft.com>
2017-06-07 11:35:27 +0300

    AuthenticationHelperTest class added

---
+++ unittest/Microsoft.WindowsAzure.MobileServices.UWP.Test/UnitTests/AuthenticationHelper.Test.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
+using Microsoft.WindowsAzure.MobileServices.TestFramework;
+
+namespace Microsoft.WindowsAzure.MobileServices.Test
+{
+    [Tag("authentication")]
+    [Tag("unit")]
+    class AuthenticationHelperTest:TestBase
+    {
+    }
+}
