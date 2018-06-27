2ccbc2d1c750dee01584fd22ed129d9628b0e13c
Ivan Matkov <v-ivmatk@microsoft.com>
2018-06-27 09:10:22 +0300

    Move RemoveSystemPropertiesKeepVersion into try/catch block

--- src/Microsoft.Azure.Mobile.Client/Table/Sync/Queue/Actions/PushAction.cs
+++ src/Microsoft.Azure.Mobile.Client/Table/Sync/Queue/Actions/PushAction.cs
@@ -170,13 +170,12 @@ namespace Microsoft.WindowsAzure.MobileServices.Sync
 
             await TryUpdateOperationState(operation, MobileServiceTableOperationState.Attempted, batch);
 
-            // strip out system properties before executing the operation
-            operation.Item = MobileServiceSyncTable.RemoveSystemPropertiesKeepVersion(operation.Item);
-
             JObject result = null;
             Exception error = null;
             try
             {
+                // strip out system properties before executing the operation
+                operation.Item = MobileServiceSyncTable.RemoveSystemPropertiesKeepVersion(operation.Item);
                 result = await batch.SyncHandler.ExecuteTableOperationAsync(operation);
             }
             catch (Exception ex)
